62cde0f715fa8ede148b6495dd05f2c20d173a43
Joorak <32602338+Joorak@users.noreply.github.com>
2024-04-12 14:19:28 +0330

    init

---
+++ Hamjoor/Program.cs
@@ -0,0 +1,12 @@
+using Hamjoor;
+using Hamjoor.Models;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddSingleton(new MatchGameModel(500,6));
+await builder.Build().RunAsync();
