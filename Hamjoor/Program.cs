2097043fcc11d88f363cabfd2456c12f44a2214a
Joorak <aliakbar.rezapour@gmail.com>
2024-05-18 06:43:54 +0330

    dynamic some setting in project appsettings file ...

--- Hamjoor/Program.cs
+++ Hamjoor/Program.cs
@@ -8,5 +8,21 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-builder.Services.AddSingleton(new MatchGameModel(500,10));
+
+int turnDelayDuration = 1000;
+int cardCount = 10;
+try
+{
+    int.TryParse(builder.Configuration["TurnDelayDuration"], out turnDelayDuration);
+    int.TryParse(builder.Configuration["CardCount"],out cardCount);
+}
+catch (Exception)
+{
+    
+}
+
+if (turnDelayDuration < 100 || turnDelayDuration > 1000) turnDelayDuration = 1000;
+if (cardCount != 8 && cardCount != 10 && cardCount != 12) cardCount = 10;
+
+builder.Services.AddSingleton(new MatchGameModel(turnDelayDuration, cardCount));
 await builder.Build().RunAsync();
--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# Hamjoor
-sipmle blazor wasm game...
+# Hamjoor - Sipmle game using Blazor WASM
+
+
+## To Run
+- Clone the Repo
+    `git clone https://github.com/Joorak/Hamjoor `
+    
+- Restore the packages (Rebuild the solution)
+    
+- Change `TurnDelayDuration` and `CardCount` values  in `appsettings.json` file in `wwwroot` folder
+
+- Run the project
+
+- Congratulations, Game is ready to start...
