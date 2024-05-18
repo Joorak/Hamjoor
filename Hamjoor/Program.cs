562ec5a1f8e2141b90e29e057ba25b541184da17
Joorak <aliakbar.rezapour@gmail.com>
2024-05-18 05:13:52 +0330

    some little UI changes...

--- Hamjoor/Program.cs
+++ Hamjoor/Program.cs
@@ -8,5 +8,5 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-builder.Services.AddSingleton(new MatchGameModel(500,6));
+builder.Services.AddSingleton(new MatchGameModel(500,10));
 await builder.Build().RunAsync();
